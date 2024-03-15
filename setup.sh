2f43d4f15e9ff22db1b69700da6f30297ae99924
tristanbasb <tristan.basbeaudou87@gmail.com>
2024-03-15 22:34:14 +0100

    modif script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Mise à jour du site web
-rm -rf /var/www/index
-mkdir /var/www/index
-mv /git/Projet-1/site_web/* /var/www/index/
+rm -rf /var/www/html
+mkdir /var/www/html
+mv /git/Projet-1/site_web/* /var/www/html/
